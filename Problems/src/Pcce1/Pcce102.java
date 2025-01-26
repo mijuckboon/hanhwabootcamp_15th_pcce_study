f61117f8e504efd496882ab0a7847c15ba404c35
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-01-27 02:50:24 +0900

    feat: add Pcce102
    - add Pcce102.md
    - add Pcce102.java

---
+++ Problems/src/Pcce1/Pcce102.java
@@ -0,0 +1,15 @@
+package Pcce1;
+
+import java.util.Scanner;
+
+public class Pcce102 { // compile 돌려볼 수 있게 이름 변경
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a;
+
+        System.out.println(b_square);
+    }
+}
