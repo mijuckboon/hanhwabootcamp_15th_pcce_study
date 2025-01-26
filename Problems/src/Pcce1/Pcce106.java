6667913340cb974673f1fa4eafa16538d4d79555
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-01-27 02:52:20 +0900

    feat: add Pcce106
    - add Pcce106.md
    - add Pcce106.java

---
+++ Problems/src/Pcce1/Pcce106.java
@@ -0,0 +1,19 @@
+package Pcce1;
+
+public class Pcce106 { // compile 돌려볼 수 있게 이름 변경
+    public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
+        int num_student = numbers.length;
+        String[] answer = new String[num_student];
+
+        for (int i = 0; i < num_student; i++) {
+            if (our_score[i] == score_list[numbers[i]-1]) { // 수정한 부분
+                answer[i] = "Same";
+            }
+            else {
+                answer[i] = "Different";
+            }
+        }
+
+        return answer;
+    }
+}
