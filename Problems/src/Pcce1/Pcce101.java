0c05450e86677984167fa023c8923a1ab43c8a68
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-01-27 02:49:33 +0900

    feat: add Pcce101
    - add Pcce101.md
    - add Pcce101.java

---
+++ Problems/src/Pcce1/Pcce101.java
@@ -0,0 +1,13 @@
+package Pcce1;
+
+public class Pcce101 { // compile 돌려볼 수 있게 이름 변경
+    public static void main(String[] args) {
+        String msg = "Spring is beginning"; // 채운 부분
+        int val1 = 3; // 채운 부분
+        String val2 = "3"; // 채운 부분
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
